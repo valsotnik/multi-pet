d254d4126a30f977b76c6969c3348378ba8f6764
Valery Sotnik <v.sotnik@andersenlab.com>
2024-05-30 20:49:30 +0200

    feat [auth]: add auth base flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from './auth/models/user.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'multi-pet';
+export class AppComponent implements OnInit {
+  readonly authService = inject(AuthService);
+  readonly http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http
+      .get<{ user: User }>('https://api.realworld.io/api/user')
+      .subscribe({
+        next: response => {
+          console.log('response', response);
+          this.authService.currentUser.set(response.user);
+        },
+        error: () => {
+          this.authService.currentUser.set(null);
+        },
+      });
+  }
+
+  logout(): void {
+    localStorage.setItem('token', '');
+    this.authService.currentUser.set(null);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register/register.component';
+import { LoginComponent } from './auth/login/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
